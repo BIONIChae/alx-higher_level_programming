ec290d88012d1b7ee31074b086a90a6e909d9487
BIONIChae <chanteldhlamini12@gmail.com>
2023-06-06 09:38:28 -0700

    Updated: Combinations

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 for num in range(0, 10):
     for dig in range(0, 10):
         if num < dig:
-            if (num == 8) and( dig == 9):
+            if (num == 8) and (dig == 9):
                 print("{:d}{:d}".format(num, dig))
             else:
                 print("{:d}{:d}".format(num, dig), end=', ')
