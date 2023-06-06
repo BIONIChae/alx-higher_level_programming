38434d33eec155e2552818f3239082589a011698
BIONIChae <chanteldhlamini12@gmail.com>
2023-06-06 12:50:34 -0700

    UPPERCASE

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uppercase(str):
+    up_str = ""
+    for val in str:
+        if "a" <= val and val <= "z":
+            caps = chr(ord(val) - ord("a") + ord("A"))
+        else:
+            caps = val
+        up_str += caps
+    print("{:s}".format(up_str), end=" ")
+    print()
