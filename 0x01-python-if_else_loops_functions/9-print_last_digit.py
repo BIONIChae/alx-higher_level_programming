75470335324e187a1ec0c6d1bcae11bfbf015e17
BIONIChae <chanteldhlamini12@gmail.com>
2023-06-06 13:06:10 -0700

    Value of the last digit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    last_d = abs(number) % 10
-    return last_d
-    print(last_d, end='')
+    end_num= abs(number) % 10
+    print(end_num, end="")
+    return end_num
