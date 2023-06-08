bf536aef7bfddddc290f8c3a4f2656a83d1fc801
BIONIChae <chanteldhlamini12@gmail.com>
2023-06-08 11:12:32 -0700

    Import

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+argv = sys.argv[1:]
+for argument in argv:
+    if argument < 0:
+        abs(argument
+    print(sum(int(argument)))
+
+
+if __name__ == '__main__':
+    pass
