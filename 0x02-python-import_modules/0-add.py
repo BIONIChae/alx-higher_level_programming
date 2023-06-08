54b859630860822835667a2fbe2542095f7fe35e
BIONIChae <chanteldhlamini12@gmail.com>
2023-06-08 05:59:50 -0700

    Add: Updated

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 import add_0 as a0
+def main():
+    a = 1
+    b = 2
 
-a = 1
-b = 2
-
-print("{:d} + {:d} =  {:d}".format(a, b, a0.add(a, b)))
+    print("{:d} + {:d} =  {:d}".format(a, b, a0.add(a, b)))
+if __name__ == '__main__':
+    main()
