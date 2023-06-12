dc78f8aebc9742431ccd473b50eb3b1689042917
BIONIChae <chanteldhlamini12@gmail.com>
2023-06-12 04:57:45 -0700

    Max number

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,8 @@
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    max_value = 0
+    for number in my_list:
+        if number > max_value:
+            max_value = number
+    return max_value
