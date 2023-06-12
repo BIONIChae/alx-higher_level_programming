ccb0e8ee637e29cead1603aedf0194cd6d0ad57c
BIONIChae <chanteldhlamini12@gmail.com>
2023-06-12 13:09:09 -0700

    Tuple2: Updated

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) == 0:
-        sentence[0] = None
+    if sentence == "":
+        return 0, None
     length = 0
     for char in sentence:
         length += 1
