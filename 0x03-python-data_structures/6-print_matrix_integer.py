185a0d5b106a947095a35d258e5f9eb3cb52ffd8
BIONIChae <chanteldhlamini12@gmail.com>
2023-06-12 01:29:02 -0700

    Updated: Matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for r in matrix:
-            for column in range(len(r)):
-                if column == len(r) - 1:
-                    print("{:d}".format(r[column]), end="")
-                else:
-                    print("{:d}".format(r[column]), end=" ")
-            print()
+        for column in range(len(r)):
+            if column == len(r) - 1:
+                print("{:d}".format(r[column]), end="")
+            else:
+                print("{:d}".format(r[column]), end=" ")
+        print()
